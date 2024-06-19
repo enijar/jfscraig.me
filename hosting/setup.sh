e4c10a055eb6d372f726df9ad1c11ff08cd52e69
Enijar <TheEnijar@gmail.com>
2024-06-19 23:07:04 +0100

    fix: setup.sh typo

--- hosting/setup.sh
+++ hosting/setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 ln -sf /var/www/jfscraig.me/hosting/nginx.conf /etc/nginx/sites-enabled/jfscraig.me
-pm2 reload /var/www/jfscraig.me/hosting/ecosystem.config
+pm2 reload /var/www/jfscraig.me/hosting/ecosystem.config.js
 service nginx reload
