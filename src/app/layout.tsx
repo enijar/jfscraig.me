770f315dfc9f67bae2943daec41d40ec11de69fc
Enijar <TheEnijar@gmail.com>
2024-06-20 23:12:12 +0100

    chore: move title and meta to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import React from "react";
-import type { Metadata } from "next";
 import * as AppStyle from "@/styles/app";
 import StyleSheet from "@/components/style-sheet";
-
-export const metadata: Metadata = {
-  title: "James FS Craig",
-  description: "Tech lead, with over a decade of digital agency experience in London",
-};
 
 type Props = {
   children: React.ReactNode;
@@ -18,6 +12,7 @@ export default function RootLayout(props: Props) {
       <head>
         <title>James FS Craig</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Tech lead, with over a decade of digital agency experience in London" />
       </head>
       <body>
         <StyleSheet>
